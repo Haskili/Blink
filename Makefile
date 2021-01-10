eda39e454250f745c2f08e8dad5835c16205ada1
Haskili <connor.adams5461994@gmail.com>
2021-01-10 14:44:57 -0800

    Update FTPD formula, formalize project structure, change setting for video backend in driver.cpp, minor changes to misc. functions...

--- Makefile
+++ Makefile
@@ -3,12 +3,18 @@ FLAGS = `pkg-config --cflags $(LIBS)`
 LINK = `pkg-config --libs $(LIBS)`
 LIBS = opencv4
 
-all: blink
+driver: driver.o blink.o
+	@echo -e "Linking 'Blink'..."
+	$(CXX) $(FLAGS) -o driver driver.o blink.o $(LINK)
 
-blink: blink.cpp blink.h
-	@echo "Compiling 'Blink'"
-	$(CXX) $(FLAGS) -o blink blink.cpp $(LINK)
+driver.o: driver.cpp blink.h
+	@echo -e "Compiling driver..."
+	$(CXX) $(FLAGS) -c -g driver.cpp
+
+blink.o: blink.cpp blink.h
+	@echo -e "Compiling library files..."
+	$(CXX) $(FLAGS) -c -g blink.cpp
 
 clean:
-	@echo "Removing executables and capture.png files from directory..."
-	rm -rf ./blink ./*.png
+	@echo "Removing object files, executables, and capture.png files from directory..."
+	rm -rf ./*.o ./driver ./*.png
---
+++ driver.cpp
@@ -0,0 +1,95 @@
+#include "blink.h"
+
+int main(int argc, char* argv[]) {
+
+	// Retrieve and validate arguments
+	CommandLineParser parser(argc, argv, args);
+	if (parser.has("help")) {
+		parser.printMessage();
+		return EXIT_SUCCESS;
+	}
+	int devID = parser.get<int>("device");
+	int blur = parser.get<int>("blur");
+	int mode = parser.get<int>("mode");
+	int tryRotate = parser.get<int>("rotate");
+	double threshold = parser.get<double>("threshold")*0.01;
+	double scale = (parser.get<double>("scale"));
+	long interval = stol(parser.get<string>("interval"), 0, 10);
+	string cascPath = parser.get<string>("classifier");
+
+	// Setup timer 'ts' for interval between captures
+	// e.g Default: 500000000ns -> 500ms -> 0.5s
+	struct timespec ts = {ts.tv_sec = 0, ts.tv_nsec = interval};
+	while (ts.tv_nsec >= 1000000000L)
+		ts.tv_sec++, ts.tv_nsec-=1000000000L;
+
+	// Open the camera and capture a single image 
+	// after performing error checking on the VideoCapture
+	Mat idIMG, curr, prev;
+	VideoCapture cap;
+	if (!cap.open(devID)) {
+		fprintf(stderr, "ERR isOpened() failed opening '%i'\n", devID);
+		return EXIT_FAILURE;
+	}
+	if (!cap.set(cv::CAP_PROP_BUFFERSIZE, 1)) {
+		fprintf(stderr, "ERR set() operation not supported by backend\n");
+		return EXIT_FAILURE;
+	}
+	cap >> prev;
+
+	// Load the classifier for identifying object(s) in frame
+	CascadeClassifier cascade;
+	if (!cascade.load(cascPath)) {
+		fprintf(stderr, "ERR load() failed loading '%s'\n", cascPath);
+		return EXIT_FAILURE;
+	}
+
+	// Enter main loop for capturing and reading
+	for (int cnum = 0;;) {
+		
+		// Wait and then capture an image from the device
+		nanosleep(&ts, NULL);
+		cap >> curr;
+		if (curr.empty()) {
+			fprintf(stderr, "ERR main() empty capture\n");
+			return EXIT_FAILURE;
+		}
+
+		// Check for invalid dimensions before comparing
+		if (curr.cols != prev.cols || curr.rows != prev.rows) {
+			fprintf(stderr, "ERR main() bad capture dims\n");
+			return EXIT_FAILURE;
+		}
+
+		// Check if the difference is significant enough
+		// to warrant a thorough difference calculation
+		if (PSNR(curr, prev) > 45.0)
+			continue;
+
+		// Calculate the difference using specified method
+		double diff = mode? SSIM(curr, prev):FTPD(curr, prev, 0.025f);
+
+		// Check if the difference percentage between the captures 
+		// excedes the threshold for 'different images'
+		if (diff >= threshold) {
+
+			// Log the event and check for identifiable objects
+			time_t ctime;
+			time(&ctime);
+			fprintf(stdout, "\n%sEvent #%i -- %4.2f%% difference\n",
+				asctime(localtime(&ctime)), cnum, diff*100);
+
+			idIMG = curr.clone();
+			fprintf(stdout, "Identified '%i' object(s)...\n\n",
+				detectObj(idIMG, cascade, scale, tryRotate, blur));
+
+			// Write the capture to a seperately saved image and
+			// reset 'prev' for the next comparison
+			char nameBuff[50];
+			sprintf(nameBuff, "capture-%i.png", cnum++);
+			imwrite(nameBuff, idIMG);
+			prev = curr.clone();
+		}
+	}
+	return EXIT_SUCCESS;
+}
